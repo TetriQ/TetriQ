0c4b1992102a46b80455b9fb75d73de6e34dd0fc
GodlyJaaaj <sebastien.lucas@epitech.eu>
2024-04-29 11:16:05 +0200

    add: license header

--- common/include/Network.hpp
+++ common/include/Network.hpp
@@ -1,8 +1,19 @@
 /*
-** EPITECH PROJECT, 2024
-** tetriq
-** File description:
-** Network.hpp
+* This file is part of TetriQ.
+* Copyright © 2024 Sébastien Lucas <sebastien.lucas@epitech.eu>
+*
+* TetriQ is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* TetriQ is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with TetriQ. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #ifndef NETWORK_HPP
