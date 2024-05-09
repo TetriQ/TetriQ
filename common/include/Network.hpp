91c272f70a34624217e742f6e434fa7c2c4667c2
GodlyJaaaj <sebastien.lucas@epitech.eu>
2024-05-09 19:26:41 +0200

    feat: add Const errors messages

--- common/include/Network.hpp
+++ common/include/Network.hpp
@@ -7,8 +7,12 @@
 #include <enet/enet.h>
 #include <iostream>
 
-namespace tetriq
-{
+namespace tetriq {
+    const std::string ENET_INIT_ERROR =
+        "An error occurred while initializing ENet.";
+    const std::string ENET_INIT_SUCCESS = "ENet initialized successfully.";
+    const std::string ENET_DEINIT_SUCCESS = "ENet deinitialized successfully.";
+
     class NetworkObject {
         public:
             virtual ~NetworkObject() = default;
