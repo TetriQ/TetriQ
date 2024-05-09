44a3e6b78593a54af7d5d528e834cc9f7663a910
GodlyJaaaj <sebastien.lucas@epitech.eu>
2024-05-09 19:26:12 +0200

    feat: add Client class, connection to server and exceptions

---
+++ client/src/ClientExceptions.cpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2024 The TetriQ authors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include <utility>
+
+#include "Client.hpp"
+
+namespace tetriq {
+    Client::ClientException::ClientException(std::string message)
+    : _message(std::move(message))
+    {}
+
+    const char *Client::ClientException::what() const noexcept
+    {
+        return _message.c_str();
+    }
+
+    Client::ClientInitException::ClientInitException()
+    : ClientException("Failed to initialize client")
+    {}
+
+    Client::ClientConnectionException::ClientConnectionException() : ClientException("Failed to connect to server")
+    {}
+}
