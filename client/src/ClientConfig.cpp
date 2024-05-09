b05dc725e441c94e900904f9f7ed3d17bab5991b
Noé Lopez <noelopez@free.fr>
2024-05-09 21:26:03 +0200

    feat: configurations loader using tomlplusplus

---
+++ client/src/ClientConfig.cpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 The TetriQ authors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "ClientConfig.hpp"
+
+tetriq::ClientConfig::ClientConfig(const std::string &config_name)
+    : AConfig(config_name)
+{}
---
+++ common/src/AConfig.cpp
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2024 The TetriQ authors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "AConfig.hpp"
+#include "Logger.hpp"
+
+#include <cstdlib>
+#include <string>
+#include <unistd.h>
+
+static std::string xdg_config_home()
+{
+    const char *home = std::getenv("XDG_CONFIG_HOME");
+    if (home == NULL)
+        home = "~/.config";
+    return home;
+}
+
+tetriq::AConfig::AConfig(const std::string &config_name)
+{
+    if (tryParse(config_name))
+        return;
+    if (tryParse("etc/" + config_name))
+        return;
+    if (tryParse(xdg_config_home() + "/tetriq/" + config_name))
+        return;
+    if (tryParse("/etc/tetriq/" + config_name))
+        return;
+    throw ConfigNotFoundException();
+}
+
+bool tetriq::AConfig::tryParse(const std::string &file_name)
+{
+    if (access(file_name.data(), R_OK) != 0) {
+        std::string msg = "looking for configuration " + file_name;
+        Logger::log(LogLevel::INFO, msg);
+        return false;
+    }
+    try {
+        _table = toml::parse_file(file_name);
+    } catch (const toml::parse_error &err) {
+        std::string msg = "failed to load config file " + file_name;
+        Logger::log(LogLevel::ERROR, msg);
+        Logger::log(LogLevel::ERROR, err.what());
+        return false;
+    }
+    Logger::log(LogLevel::INFO, "using configuration " + file_name);
+    return true;
+}
