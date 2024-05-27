e11bdd51a6865013e93f96ab0492532f3c34baf3
Noé Lopez <noelopez@free.fr>
2024-05-27 14:51:57 +0200

    fix: move packets in correct directory

--- common/src/network/packets/TestPacket.cpp
+++ common/src/network/packets/TestPacket.cpp
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-#include "network/TestPacket.hpp"
+#include "network/packets/TestPacket.hpp"
 #include "Logger.hpp"
 #include "network/NetworkStream.hpp"
 #include "network/PacketId.hpp"
