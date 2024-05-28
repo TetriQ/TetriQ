fd9038ae4de20b188080f48b25e8f50ccb21cb99
Noé Lopez <noelopez@free.fr>
2024-05-28 12:32:53 +0200

    feat: parse server ip and port in client

---
+++ client/src/Arguments.cpp
@@ -0,0 +1,25 @@
+#include "Arguments.hpp"
+#include "Logger.hpp"
+#include <stdexcept>
+
+namespace tetriq {
+    bool Arguments::parse(int argc, char *argv[])
+    {
+        if (argc != 3) {
+            LogLevel::ERROR << "invalid number of arguments" << std::endl;
+            return false;
+        }
+        ip = argv[1];
+        try {
+            int n = std::stoi(argv[2]);
+            if ((uint16_t) n != n) {
+                throw std::out_of_range("");
+            }
+            port = n;
+        } catch (std::logic_error) {
+            LogLevel::ERROR << "invalid port number" << std::endl;
+            return false;
+        }
+        return true;
+    }
+}
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -4,21 +4,27 @@
 
 #include "Client.hpp"
 #include "IDisplay.hpp"
+#include "Logger.hpp"
 #include "SFMLDisplay.hpp"
+#include "Arguments.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
+    tetriq::Arguments args;
+    if (!args.parse(argc, argv)) {
+        tetriq::LogLevel::INFO << "usage: ./tetriq_client ip port" << std::endl;
+        return EXIT_FAILURE;
+    }
 
     // Seed the random number generator
     srand(time(nullptr));
 
     try {
         std::unique_ptr<tetriq::IDisplay> display{new tetriq::SFMLDisplay{}};
-        tetriq::Client client("localhost", 31457, *display);
+        tetriq::Client client(args.ip, args.port, *display);
         client.loop();
     } catch (const tetriq::Client::ClientInitException &e) {
         std::cerr << e.what() << std::endl;
